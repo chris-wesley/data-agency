b83bdd07e7a5ab6446b02521e4e4baf11278b298
chris-wesley <57838883+chris-wesley@users.noreply.github.com>
2020-04-29 16:14:20 +0100

    Added functional scoring

--- js/main.js
+++ js/main.js
@@ -2,17 +2,48 @@
 const sections = document.querySelectorAll('.section');
 const cards = document.querySelectorAll('.card');
 
-const draggable = new Draggable.default(document.querySelectorAll('.container'), {
-    draggable: '.card',
+const draggableSource = new Draggable.default(document.querySelectorAll('.sources'), {
+    draggable: '.source',
     mirror: {
         appendTo: 'body',
         constrainDimensions: true,
     }
 });
 
-draggable.on('drag:start', () => console.log('drag:start'));
-draggable.on('drag:move', () => console.log('drag:move'));
-draggable.on('drag:stop', () => console.log('drag:stop'));
+draggableSource.on('drag:start', (event) => {
+    draggableSourceOrigin = event.originalSource.dataset.dropzone;
+    originalSource = event.originalSource;
+    console.log('drag:start');
+});
+
+draggableSource.on('drag:move', () => console.log('drag:move'));
+
+draggableSource.on('drag:over', () => console.log('drag:over'));
+
+draggableSource.on('drag:over:container', () => console.log('drag:over:container'));
+
+draggableSource.on('drag:out:container', () => {
+	console.log('drag:out:container');
+	}
+);
+
+draggableSource.on('drag:stop', () => {
+	console.log('drag:stop');
+		buySource(originalSource);
+
+    }
+);
+
+draggableSource.on('mirror:create', () => console.log('mirror:create'));
+
+draggableSource.on('mirror:created', () => console.log('mirror:created'));
+
+draggableSource.on('mirror:attached', () => console.log('mirror:attached'));
+
+draggableSource.on('mirror:move', () => console.log('mirror:move'));
+
+draggableSource.on('mirror:destroy', () => console.log('mirror:destroy'));
+
 
 // Check the card is dropping in the right section
 function cardRules() {
@@ -116,6 +147,24 @@ async function getClients() {
             return str.indexOf(randomName) === -1;
         });
     }
+}
+
+function buySource(element) {
+	console.log((element).textContent);
+	var price = (element).textContent;
+	reduceScore(price);
+	(element).textContent = "SOLD";
+	(element).style.backgroundColor = "black";
+	(element).style.color = "white";
+}
+
+function reduceScore(price) {
+	var scoreElement = document.getElementById("score");
+	var scoreString = scoreElement.textContent.replace("$", "");
+	var scoreInt = parseInt(scoreString);
+	var priceString = (price).replace ( /[^0-9]/g, '' );
+	var priceInt = parseInt(priceString);
+	scoreElement.textContent = ("$" + (scoreString - priceString));
 }
 
 // 60 second countdown
